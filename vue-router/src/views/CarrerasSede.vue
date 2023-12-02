160fd8758e32764d238b843bba500b8df56bcda2
Luciano Garriga <lucianomgarriga@gmail.com>
2023-12-02 12:46:58 -0300

    Add info in home section / Add styles / Directives v-in & v-for

--- vue-router/src/views/CarrerasSede.vue
+++ vue-router/src/views/CarrerasSede.vue
@@ -1,28 +1,37 @@
 <template>
     <h1>Carreras por Sede</h1>
     <div id='container'>
+        <!-- Renderizar el id que envian por url -->
         <h1>{{$route.params.id}}</h1>
+        <!-- Renderizar la sede -->
         <h4>{{$route.params.sede}}</h4>
-        <div v-if="sede===1">
+
+        <div v-if="sede==1">
             <p v-for='(carrera,i) in sede1' :key="i">{{carrera}} </p>
         </div>
-        <div v-if="sede===2">
+        <div v-if="sede==2">
             <p v-for='(carrera,i) in sede2' :key="i">{{carrera}} </p>
         </div>
-        <div v-if="sede===3">
+        <div v-if="sede==3">
             <p v-for='(carrera,i) in sede3' :key="i">{{carrera}} </p>
         </div>
-        <div v-if="sede===4">
+        <div v-if="sede==4">
             <p v-for='(carrera,i) in sede4' :key="i">{{carrera}} </p>
         </div>
+
     </div>
 </template>
 
 <script>
 export default {
     name: 'CarrerasSede',
+    // computed:{
+    //     sede() {
+    //         return this.$route.params.id;
+    //     }
+    // },
     data(){
-        return{
+        return{ 
             sede: this.$route.params.id,
             sede1: ['Ing. Civil', 'Arquitectura', 'Redes Informaticas'],
             sede2: ['Gastronomia', 'Mecatronica', 'Desarrollo de Software'],
--- vue-router/src/views/InicioView.vue
+++ vue-router/src/views/InicioView.vue
@@ -1,14 +1,33 @@
-<template>  
-    <img
-      src="https://assets-global.website-files.com/63a9fb94e473f36dbe99c1b1/651bc8a55cd18b44ca69f0be_WQyiBeQTeiDdoyXRXw5B.png"
-      alt="logo"
-    />
-    <h1>Inicio</h1>
+<template>
+  <img
+    src="https://assets-global.website-files.com/63a9fb94e473f36dbe99c1b1/651bc8a55cd18b44ca69f0be_WQyiBeQTeiDdoyXRXw5B.png"
+    alt="logo"
+  />
+  <h1>Bienvenido</h1>
+  <div class="container">
+    <h2>Proyecto Vue-Router</h2>
+    <p class="subtitle">Utilización de tecnolog&iacute;as como:</p>
+    <ul v-for="(item, index) in tech" :key="index">
+      <p>- {{item}}</p>
+    </ul>
+    <hr><br>
+    
+    <p class="subtitle">Utilización de directivas como:</p>
+    <ul v-for="(item, index) in directivas" :key="index">
+      <p>- {{item}}</p> 
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
   name: "InicioView",
+  data(){
+    return{
+      tech: ['Rutas', 'Router-link', 'Rarámetros de URL', 'Rutas Dinámicas'],
+      directivas: ['v-for','v-if']
+    }
+  }
 };
 </script>
 
@@ -18,5 +37,18 @@ img {
   max-width: 550px;
   padding: 20px;
   margin-top: 25px;
+}
+.container{
+  margin: auto;
+  width: 80%;
+  max-width: 550px; 
+  border: 2px solid salmon;
+  border-radius: 10px;
+  margin-bottom: 40px;
+}
+.subtitle{
+  color: darkblue;
+  font-size: 18px;
+  font-weight: 600;
 }
 </style>

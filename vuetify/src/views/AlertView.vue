62b9e0b2e638a92e32c07b12fe61925f8f1b2589
Luciano Garriga <lucianomgarriga@gmail.com>
2023-12-10 20:45:54 -0300

    Add alert example with v-alert info

--- vuetify/src/views/AlertView.vue
+++ vuetify/src/views/AlertView.vue
@@ -1,12 +1,37 @@
 <template>
     <v-container :fluid="true">
         <h1 class="text-green-darken-4">Alert</h1>
+        <v-btn
+            prepend-icon="mdi-bell-ring"
+            color="indigo"
+            rounded="xl"
+            @click="alertState = !alertState"
+        >
+        Mostrar alerta
+        </v-btn>
+        <br>
+        <!-- Alerta -->
+        <v-alert
+            closable
+            v-model="alertState"
+            icon="mdi-information"
+            title="Respuesta"
+            text="El producto ha sido guardado" 
+            class="mt-3"
+        >
+
+        </v-alert>
     </v-container>
 </template>
 
 <script>
 export default {
-    name: 'AlertView'
+    name: 'AlertView',
+    data(){
+        return {
+            alertState: false
+        }
+    }
 }
 </script>
 

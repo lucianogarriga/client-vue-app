88bf1e228c3cfae97d927fa5177e1c240fe54f0c
Luciano Garriga <lucianomgarriga@gmail.com>
2023-12-10 21:04:35 -0300

    Add examples / Tabs and Table components with Vuetify

--- vuetify/src/views/TableView.vue
+++ vuetify/src/views/TableView.vue
@@ -1,15 +1,36 @@
 <template>
-    <div> 
-        <h1 class="text-black-4">Table</h1>
-    </div>
+  <v-container :fluid="true">
+    <h1 class="text-black-4">Table</h1>
+    <v-table hover>
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Apellido</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+            <td>Luciano</td>
+            <td>Garriga</td>
+            <td><v-btn icon="mdi-close" size="small" color="red"></v-btn></td>
+        </tr>
+        <tr>
+            <td>Francisco</td>
+            <td>Gomez</td>
+            <td><v-btn icon="mdi-pencil" size="small" color="blue"></v-btn></td>
+        </tr>
+      </tbody>
+    </v-table>
+  </v-container>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style scoped>
-
+.v-container {
+  max-width: 890px;
+}
 </style>
--- vuetify/src/views/TabsView.vue
+++ vuetify/src/views/TabsView.vue
@@ -1,15 +1,33 @@
-<template> 
-    <v-container :fluid="true">
-        <h1 class="text-black-4">Tabs</h1>
-    </v-container>
+<template>
+  <v-container :fluid="true">
+    <h1 class="text-black-4">Tabs</h1>
+    <!-- Pestañas / Tabs -->
+    <v-tabs v-model="tabs" bg-color="indigo-darken-4">
+      <v-tab value="1">Tab 1</v-tab>
+      <v-tab value="2">Tab 2</v-tab>
+      <v-tab value="3">Tab 3</v-tab>
+    </v-tabs>
+
+    <!-- Contenido de las Ventanas -->
+    <v-window v-model="tabs">
+      <v-window-item value="1">Contenido de Pestaña 1</v-window-item>
+      <v-window-item value="2">Contenido de Pestaña 2</v-window-item>
+      <v-window-item value="3">Contenido de Pestaña 3</v-window-item>
+    </v-window>
+  </v-container>
 </template>
 
 <script>
 export default {
-    name: 'TabsView'
-}
+  name: "TabsView",
+  data() {
+    return {
+      // Para determinar la Pestaña activa
+      tabs: null,
+    };
+  },
+};
 </script>
 
 <style scoped>
-
 </style>

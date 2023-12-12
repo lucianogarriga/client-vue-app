ee84b50b1c740df007ccc27fdf2c4abd1cf922ba
Luciano Garriga <lucianomgarriga@gmail.com>
2023-12-12 00:06:38 -0300

    Add table in EquipoView - Add methods to create and delete members

--- localstorage/src/views/ColoresView.vue
+++ localstorage/src/views/ColoresView.vue
@@ -7,15 +7,21 @@
           label="Color"
           placeholder="Ingresa un color"
           color="purple"
+          v-model="color"
         >
         </v-text-field>
-        <v-btn prepend-icon="mdi-check" color="purple-darken-2">Guardar Color</v-btn>
+        <v-btn
+          prepend-icon="mdi-check"
+          color="purple-darken-2"
+          @click="agregarColor"
+          >Guardar Color</v-btn
+        >
       </v-col>
       <v-col>
-        <v-card title="Color" color="deep-purple-lighten-4">
-            <v-card-actions>
-                <v-btn icon="mdi-delete" color="red"></v-btn>
-            </v-card-actions>
+        <v-card title="Color" color="deep-purple-lighten-4" :text="colorGuardado" v-if="colorGuardado !==''">
+          <v-card-actions>
+            <v-btn icon="mdi-delete" color="red" @click="eliminarColor"></v-btn>
+          </v-card-actions>
         </v-card>
       </v-col>
     </v-row>
@@ -25,12 +31,45 @@
 <script>
 export default {
   name: "ColoresView",
+  data() {
+    return {
+      // Variable p/ guardar color que ingrese x text-box
+      color: "",
+      // Variable p/ guardar color obtenido del localStorage
+      colorGuardado: "",
+    };
+  },
+  methods: {
+    // Metodo que agrega colores al localStorage
+    agregarColor() {
+      // Agregar item a LS
+      localStorage.setItem("color", this.color);
+      // Invocar metodo p/ leer color
+      this.leerColor();
+      // Vaciar la variable color
+      this.color = "";
+    },
+    leerColor() {
+      // Obtener item del LS
+      let data = localStorage.getItem("color");
+      // Verificacion
+      data ? (this.colorGuardado = data) : (this.colorGuardado = "");
+    },
+    eliminarColor() {
+      localStorage.removeItem("color");
+      this.leerColor();
+    },
+    created(){
+      // Extraer datos del LS
+      this.leerColor();
+    }
+  },
 };
 </script>
 
 <style>
-.v-container{
-    max-width: 990px;
-    margin: 0 auto;
+.v-container {
+  max-width: 990px;
+  margin: 0 auto;
 }
 </style>
--- localstorage/src/views/EquipoView.vue
+++ localstorage/src/views/EquipoView.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <h1 class="text-purple-darken-2 mb-3" >Estudiante</h1>
+    <h1 class="text-purple-darken-2 mb-3">Estudiante</h1>
     <v-row>
       <v-col>
         <v-text-field
@@ -8,38 +8,64 @@
           placeholder="Ingrese su nombre"
           color="purple"
           clearable
+          v-model="nombre"
         ></v-text-field>
-      <v-text-field
-        label="Edad"
-        placeholder="Ingrese su edad"
-        color="purple"
-        type="number"
-        clearable 
-      ></v-text-field>
-      <v-btn block color="purple-darken-2" prepend-icon="mdi-check"
-        >Guardar datos</v-btn >
+        <v-text-field
+          label="Edad"
+          placeholder="Ingrese su edad"
+          color="purple"
+          type="number"
+          clearable
+          v-model="edad"
+        ></v-text-field>
+        <v-btn
+          block
+          color="purple-darken-2"
+          prepend-icon="mdi-check"
+          @click="guardarDatos"
+          >Guardar datos</v-btn
+        >
       </v-col>
-      <v-col> 
+      <v-col>
         <v-card title="Equipo">
-            <v-card-text>
-                <v-btn append-icon="mdi-delete-forever" color="red" size="small">Borrar todo</v-btn>
-                <v-table>
-                    <thead>
-                        <tr>
-                            <th>Nombre</th>
-                            <th>Edad</th>
-                            <th>Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Luciano</td>
-                            <td>29</td>
-                            <td><v-btn icon="mdi-delete" size="x-small" color="red"></v-btn></td>
-                        </tr>
-                    </tbody>
-                </v-table>
-            </v-card-text>
+          <v-card-text>
+            <v-btn
+              append-icon="mdi-delete-forever"
+              color="red"
+              size="small"
+              @click="eliminarTodo"
+              v-if="equipo.length !== 0"
+              >Borrar todo</v-btn
+            >
+            <v-table>
+              <thead>
+                <tr>
+                  <th>Nombre</th>
+                  <th>Edad</th>
+                  <th>Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(persona, index) in equipo" :key="index">
+                  <td>{{ persona.nombre }}</td>
+                  <td>{{ persona.edad }}</td>
+                  <td>
+                    <v-btn
+                      icon="mdi-delete"
+                      size="x-small"
+                      color="red"
+                      @click="eliminar(index)"
+                    ></v-btn>
+                  </td>
+                </tr>
+                <tr v-if="equipo.length === 0">
+                  <td colspan="3" class="text-center">
+                    No hay estudiantes cargados
+                  </td>
+                </tr>
+              </tbody>
+            </v-table>
+          </v-card-text>
         </v-card>
       </v-col>
     </v-row>
@@ -49,6 +75,51 @@
 <script>
 export default {
   name: "EquipoView",
+  data() {
+    return {
+      // Var p/ almacenar datos del Form
+      nombre: "",
+      edad: ' ',
+      // Arreglo de estudiantes
+      equipo: [],
+    };
+  },
+  methods: {
+    // Almacenar datos en el LS
+    guardarDatos() {
+      let persona = { nombre: this.nombre, edad: this.edad };
+      // Agregar Persona al Array (Equipo)
+      this.equipo.push(persona);
+      // Agregar Array al LS
+      localStorage.setItem("equipo", JSON.stringify(this.equipo));
+      console.log(persona);
+      // Limpiar variables del Formulario
+      this.nombre = "";
+      this.edad = '' ;
+    },
+    leer() {
+      // Obtener item del LS
+      let data = JSON.parse(localStorage.getItem("equipo"));
+      // Verificar
+      if (data) {
+        this.equipo = data;
+      }
+    },
+    created() {
+      this.leer();
+    },
+    eliminarTodo() {
+      localStorage.removeItem("equipo");
+      this.equipo = [];
+      console.log(this.equipo);
+    },
+    eliminar(index) {
+      // Eliminar el estudiante x 1
+      this.equipo.splice(index, 1);
+      // Actualizar en LS
+      localStorage.setItem("equipo", JSON.stringify(this.equipo));
+    },
+  },
 };
 </script>
 
